1ff8c07bd078a921b1a8c26e47ef9a75515c9894
condaguru <jakov.markic358@gmail.com>
2026-03-23 20:00:00 +0100

    pozdrav1

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezba1_condic
+{
+    internal class Class1
+    {
+        int a;
+        int b;
+    }
+}
